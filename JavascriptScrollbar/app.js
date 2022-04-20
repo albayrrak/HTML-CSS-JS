778fec38258228d938e2233b95a5deddd77a25f9
Yücel Albayrak <albayrrak@gmail.com>
2022-04-20 15:21:14 +0300

    ScrollBar Finish

--- JavascriptScrollbar/app.js
+++ JavascriptScrollbar/app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+window.addEventListener('scroll', () => {
+    let contents = document.querySelectorAll('p')
+    contents.forEach(content => {
+        let contentPosition = content.getBoundingClientRect().top
+        console.log(contentPosition);
+        let screenPosition = window.innerHeight
+        if (contentPosition < screenPosition) {
+            let paragraph = document.querySelectorAll('.paragraph')
+            paragraph.forEach(paragraph => paragraph.classList.add('active'))
+        } else {
+            return
+        }
+    })
+
+})
